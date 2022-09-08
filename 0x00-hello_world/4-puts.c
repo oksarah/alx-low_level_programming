8989783d87b7b9b1639ab77774499b2adc4dd252
Sarah <oksarah912@gmail.com>
2022-09-09 02:08:17 +0300

    testing betty style documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * @brief this program prints the written print inside the puts function
+ * 
+ * @return 0
+ */
 int main (void)
 {
     puts("\"Programming is like building a multilingual puzzle");
