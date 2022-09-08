f74f1a84ceef642129fa8b62708af95e5c09d953
Sarah <oksarah912@gmail.com>
2022-09-09 02:18:43 +0300

    testsing a fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,7 @@
 /**
  * main - this function prinst the string input
  * 
- * Description: the main function is used
- * the program will print "Programming is like building a multilingual puzzle"
+ * Description: the main function is used the program will print "Programming is like building a multilingual puzzle"
  * Return: 0
  */
 int main (void)
