8ccf18128b43f94e68ecafad736d826f7dca67ba
Sarah <oksarah912@gmail.com>
2022-09-10 01:27:07 +0300

    printing all single numbers

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,7 +5,7 @@
 /**
  * main - print if the number is positive, zero, or negative
  * Description: using the main function
- * This program prints "programming is positive, xero, or negative"
+ * This program prints "programming is positive, zero, or negative"
  * Return: 0
  */
 int main(void)
