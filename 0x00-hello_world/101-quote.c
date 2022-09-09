e42e62a4aca97c82e829817e7a9b3638f833ae8e
Sarah <oksarah912@gmail.com>
2022-09-09 03:13:00 +0300

    advanced task 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this function prints a string with print f
+ * Description: the rpogram prints a coll statement
+ * Return: 0
+ */
+int main(void)
+{
+printf('"and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+return (1);
+}
