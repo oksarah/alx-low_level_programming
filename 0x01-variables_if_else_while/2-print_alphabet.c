140bbf7952dc6d5ce22190573e1b6a39b4b15f18
Sarah <oksarah912@gmail.com>
2022-09-09 21:31:20 +0300

    printing the alphabetsin lowercase

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,4 +2,20 @@
 #include <stdlib.h>
 #include <time.h>
 
-
+/**
+ * main - print if the number is positive, zero, or negative
+ * 
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negaative"
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+}
+putchar('\n')
+return (0);
+}
