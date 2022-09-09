8be537ab399629b25b6000766affa5f36d8f9448
Sarah <oksarah912@gmail.com>
2022-09-09 23:36:24 +0300

    commiting task 4 and 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print if the number is positive, zero, or negative
+ * Description: using the main function
+ * this program prints "programming is positive, zero, or negative"
+ * Return: 0
+ */
+int main(void)
+{
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+if (ch != 'e' && ch != 'q')
+}
+{
+putchar(ch);
+}
+{
+putchar('\n')
+}
+return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print if the number is positive, zero, or negative 
+ * Descripption: using the main function
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%i", i); 
+}
+printf("\n");
+return (0);
+}
