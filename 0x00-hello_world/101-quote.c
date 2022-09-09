585321dbc351b07ec271916043468da24bad02c9
Sarah <oksarah912@gmail.com>
2022-09-09 03:20:08 +0300

    eak fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
- * main - this function prints a string with print f
- * Description: the rpogram prints a coll statement
- * Return: 0
+ * main - this function prints a string with write
+ * Description: the pogram prints a cool statement
+ * Return: 1
  */
 int main(void)
 {
-printf('"and that piece of art is useful" - Dora Korpar, 2015-10-19\n');
+write(2, "and that piece of art is useful\n" - Dora Korpar, 2015-10-19\n",59);
 return (1);
 }
