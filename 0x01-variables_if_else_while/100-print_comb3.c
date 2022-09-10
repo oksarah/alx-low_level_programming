ed1504c7b51c741ef4f1ec38334219e5f855d86f
Sarah <oksarah912@gmail.com>
2022-09-10 17:28:43 +0300

    New fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,8 +11,8 @@
 int main(void)
 {
 int i, j;
-for ( i = 0; i <= 9; i++)
-for (j = 1; j <= 9; j++)
+for ( i = '0' ; i <= '9' ; i++)
+for (j = '1' ; j <= '9' ; j++)
 if (j > i)
 {
 putchar(i + '0');
