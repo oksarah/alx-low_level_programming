4ebae1d502825b7df73a5889279c0314099c7b36
Sarah <oksarah912@gmail.com>
2022-09-14 22:25:12 +0300

    correcting 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,18 +6,18 @@
  * @n: the number starts counting
  * Return: 0
  */
-void print_to_98(int n)
+void print_to_98(98)
 {
 if (n >= 98)
 {
 while (n > 98)
 printf("%d, ", n--);
 printf("%d\n", n);
+}
 else
 {
 while (n < 98)
 printf("%d, ", n++);
-printf("%d\n", n);
-}
+printf("%d\n", n)
 }
 }
