4fa25d43387f0019ac54196e296b0247f0e76873
Sarah <oksarah912@gmail.com>
2022-09-14 18:37:14 +0300

    new

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,25 +2,11 @@
 #define HOLBERTON_H_INCLUDED
 
 /**
- * _putchar - printing in lowercase
- * Description: using the main function
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  * Return:0  
  */
 int _putchar(char);
-/**
- * print_alphabet - in lowercase
- * Declare: using main function
- * Return: Always 0
- */
-int print_alphabet(int c);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void ptint_to_98(int n);
-void print_times_table(int n);
-void num(int r, int n);
+{
+return(0);
+}
