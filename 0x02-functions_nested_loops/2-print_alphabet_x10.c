53135b64edbe5b6145c8276810d92d8e40c3ed8b
Sarah <oksarah912@gmail.com>
2022-09-14 19:51:06 +0300

    submitting task 2, 3, 4

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - function printing 10 times the alphabet in abcd
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <=9; i++)
+
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _islower - function to check for lowercase character
+ * @c: is the int that will be used for the argument
+ * Return: 0
+ */
+int _islowe(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+return (0);
+}
