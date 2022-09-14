6554d1f4be4c47b5103382b3936b86ccdff0bce1
Sarah <oksarah912@gmail.com>
2022-09-14 23:04:39 +0300

    africa fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,32 +6,32 @@
  * @n: the number starts counting
  * Return: 0
  */
-int print_to_98(int n)
-{
-if (n >= 98)
-{
-while (n > 98)
-{
-printf("%d, ", n--);
-printf("%d\n", n);
-}
-}
-else
-{
-while (n < 98)
-{
-printf("%d, ", n++);
-printf("%d\n", n);
-}
-}
-}
+// int print_to_98(int n)
+// {
+// if (n >= 98)
+// {
+// while (n > 98)
+// {
+// printf("%d, ", n--);
+// printf("%d\n", n);
+// }
+// }
+// else
+// {
+// while (n < 98)
+// {
+// printf("%d, ", n++);
+// printf("%d\n", n);
+// }
+// }
+// }
 
-int main(void)
-{
-print_to_98(0);
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(-10);
-return (0);
-}
+// int main(void)
+// {
+// print_to_98(0);
+// print_to_98(98);
+// print_to_98(111);
+// print_to_98(81);
+// print_to_98(-10);
+// return (0);
+// }
