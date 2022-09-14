5e817c61aa7591221a1a4db8faf83e79a4bfab81
Sarah <oksarah912@gmail.com>
2022-09-14 23:24:39 +0300

    the fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,8 +6,17 @@
  * @n: the number starts counting
  * Return: 0
  */
+extern void print_to_98(int n)
 int main()
 {
+print_to_98(98);
+print_to_98(111);
+print_to_98(81);
+print_to_98(0);
+print_to_98(-98);
+print_to_98(-111);
+return (0);
+}
 void print_to_98(int n)
 {
 if (n >= 98)
@@ -30,12 +39,4 @@ else
     
 }
 
-}
-print_to_98(98);
-print_to_98(111);
-print_to_98(81);
-print_to_98(0);
-print_to_98(-98);
-print_to_98(-111);
-return (0);
 }
