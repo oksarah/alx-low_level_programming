b5c825f15cecdd739dcd769e21974d6e23090c85
Sarah <oksarah912@gmail.com>
2022-09-15 16:01:36 +0300

    task 1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - tests function that prints if interger is positive or negative
  * Retuen: 0
  */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
