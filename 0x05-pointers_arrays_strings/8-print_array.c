fce399694babcd7d2a60f20c57e0c537b632b481
Sarah <oksarah912@gmail.com>
2022-09-19 19:38:40 +0300

    3 tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,7 +7,7 @@
  * @b: number of elements
  * Return: Always 0.
  */
-void print_array(int *a, int n);
+void print_array(int *a, int *n)
 {
 int inc;
 
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 int inc = 0;
 
-while (*(scr + inc) != '\0')
+while (*(src + inc) != '\0')
 {
-*(dest + inc) = *(scr + inc);
-inc++
+*(dest + inc) = *(src + inc);
+inc++;
 }
 *(dest + inc) = '\0';
 
