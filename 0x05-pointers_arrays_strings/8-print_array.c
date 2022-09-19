fe66ced905c84ca0af67d5cb8354f6bfc760b761
Sarah <oksarah912@gmail.com>
2022-09-19 19:28:33 +0300

    3 tasks

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,1 +1,23 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * print_array - prints n element of array
+ * @a: array
+ * @b: number of elements
+ * Return: Always 0.
+ */
+void print_array(int *a, int n);
+{
+int inc;
+
+int inc;
+for (inc = 0; inc < n ; inc++)
+{
+if (inc != n - 1)
+printf("%d, ", a[inc]);
+else
+printf("%d", a[inc]);
+}
+putchar(10);
+}
--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,1 +1,22 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ * _strcpy - copy paste string
+ * @dest: destination
+ * @src: source
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int inc = 0;
+
+while (*(scr + inc) != '\0')
+{
+*(dest + inc) = *(scr + inc);
+inc++
+}
+*(dest + inc) = '\0';
+
+return (dest);
+}
