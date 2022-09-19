48930f2bd49f042f630d5b0905ab1ac5f00db865
Sarah <oksarah912@gmail.com>
2022-09-19 22:23:20 +0300

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,14 +20,13 @@ while (*temp != '\0' && (*temp < '0' || *temp > '9'))
 if (*temp == '-')
 sign *= -1;
 temp++;
-{
+}
 if (*temp != '\0')
-
+{
 num = num * 10 + (*temp - '0');
 temp++;
-
+}
 while (*temp >= '0' && *temp <= '9');
 return (num * sign);
 }
-}
-}
+
