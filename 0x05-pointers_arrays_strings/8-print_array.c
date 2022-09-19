51493854b7f30ba0dc348b7bb301721a7e99591e
Sarah <oksarah912@gmail.com>
2022-09-19 20:28:27 +0300

    refix

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,13 +7,13 @@
  * @b: number of elements
  * Return: Always 0.
  */
-void print_array(int *a, int *n)
+void print_array(int *a, int n)
 {
 int inc;
 
-for (inc = 0; inc < *n ; inc++)
+for (inc = 0; inc < n ; inc++)
 {
-if (inc != *n - 1)
+if (inc != n - 1)
 printf("%d, ", a[inc]);
 else
 printf("%d", a[inc]);
