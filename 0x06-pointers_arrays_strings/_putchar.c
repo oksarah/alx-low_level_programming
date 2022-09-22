2132f2b56aeed9233cec6b86dde8ce851266efb7
Sarah <oksarah912@gmail.com>
2022-09-23 01:30:36 +0300

    task 0

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: character to print
+ * Return: always 0
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
