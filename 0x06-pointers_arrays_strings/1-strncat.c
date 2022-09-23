8dcb412f7a36ec2edca12834fb14a3ed66a7e1ca
Sarah <oksarah912@gmail.com>
2022-09-23 08:50:46 +0300

    new fix

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strncat - uses two strings  from src
@@ -7,14 +8,14 @@
  * @n: number of bytes from src to be appended to dest
  * Return: Always 0.
  */
-char *_strncat(char *dest, char *src, int n);
+char *_strncat(char *dest, char *src, int n)
 {
 int index = 0, dest_len = 0;
 
 while (dest[index++])
 dest_len++;
 
-for (inde = 0; src[index] && index < n; index++)
+for (inde = 0; src[index]; index++)
 dest[dest_len++] = src[index];
 
 return (dest);
