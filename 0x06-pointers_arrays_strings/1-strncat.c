bdb14b8f94811c6203119acbc885f4ef45d349ea
Sarah <oksarah912@gmail.com>
2022-09-23 09:10:27 +0300

    correcting 1,2,3

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,7 +17,7 @@ int index = 0, dest_len = 0;
 while (dest[index++])
 dest_len++;
 
-for (inde = 0; src[index] && index < n; index++)
+for (index = 0; src[index] && index < n; index++)
 dest[dest_len++] = src[index];
 
 return (dest);
