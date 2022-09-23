243f8224c1d8c3af4fd2c31b04358a752a9f8719
Sarah <oksarah912@gmail.com>
2022-09-23 09:42:59 +0300

    task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -7,13 +7,31 @@
  * @s2: points to the second string
  * Return: Always 0.
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2 &&*s1 == *s2)
-{
-s1++;
-s2++;
-}
+int i = 0, diff = 0;
 
-return (*s1 - *s2);
+while (1)
+{
+if (s1[i] == '\0' && s2[i] == '\0')
+break;
+else if (s1[i] == '\0')
+{
+diff = s2[i];
+break;
+}
+else if (s2[i] == '\0')
+{
+diff = s1[i];
+break;
+}
+else if (s1[i] != s2[i])
+{
+diff = s1[i] - s2[i];
+break;
+}
+else
+i++;
+}
+return (diff);
 }
