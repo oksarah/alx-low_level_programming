52fe6fc2cb80d5578481f3840502d6183c258001
Sarah <oksarah912@gmail.com>
2022-09-23 09:22:36 +0300

    correcting 1,2,3

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,8 +2,10 @@
 #include <stdio.h>
 
 /**
- * _strncpy - strings, overrites the  terminal then adds null byte
- * @dest: points to the string
+ * _strncpy - c function that copies a string
+ * if the length of the source string is less than maximum byte no
+ * reminder of the desination string is filled with null bytes
+ * @dest: stores the string copy
  * @src: sources the string to the dest
  * @n: max number of byte copied
  * Return: returns
