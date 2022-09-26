4dc6729bbeee5ebb0afadf10d9aeba5869c768e0
Sarah <oksarah912@gmail.com>
2022-09-27 00:22:20 +0300

    all tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * *_memset - fills a memory block with a constant byte
  * @s: memory block's address
@@ -8,7 +9,7 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 int i = 0;
-for (; n > 0; i++)
+for (; n > 0; i++);
 {
 s[i] = b;
 n--;
