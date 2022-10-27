ba3904b0682b4764927825bd5d33ed4d2e2b3397
oksarah <94914879+oksarah@users.noreply.github.com>
2022-10-27 16:56:41 +0300

    Update 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -22,17 +22,3 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(copy);
 		return (1);
 	}
-
-	for (node = 0; node < (index - 1); node++)
-	{
-		if (copy->next == NULL)
-			return (-1);
-
-		copy = copy->next;
-	}
-
-	tmp = copy->next;
-	copy->next = tmp->next;
-	free(tmp);
-	return (1);
-}
