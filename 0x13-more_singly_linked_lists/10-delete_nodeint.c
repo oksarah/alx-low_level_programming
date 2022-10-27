32596dac9b634bb2a14a813dcd74e7d72e30dfb7
Sarah <oksarah912@gmail.com>
2022-10-27 19:05:01 +0300

    Fix

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,8 +5,8 @@
 
 /**
  /**
- * delete_nodeint_at_index - deletes a node in a linked list at a certain index
- * @head: pointer to the first element in the list
+ * delete_nodeint_at_index - deletes a node in a linked list of a certain index
+ * @head: points to the first element in the list
  * @index: index of the node to delete
  * Return: 1 (Success), or -1 (Fail)
  */
