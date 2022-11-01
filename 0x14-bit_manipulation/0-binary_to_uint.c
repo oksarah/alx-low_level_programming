95934deb372bf1ff2b707a1f0234badcc22f1dbf
Sarah <oksarah912@gmail.com>
2022-11-01 07:49:33 +0300

    fixing a fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    int i;
-    unsigned int dec_val = 0;
+	unsigned int num = 0;
+	int len = 0;
 
-    if (!b)
-        return (0);
+	if (b[len] == '\0')
+		return (0);
 
-    for (i = 0; b[i]; i++)
-    {
-        if (b[i] < '0' || b[i] > '1')
-            return (0);
-        dec_val = 2 * dec_val + (b[i] - '0');
-    }
+	while ((b[len] == '0') || (b[len] == '1'))
+	{
+		num <<= 1;
+		num += b[len] - '0';
+		len++;
+	}
 
-    return (dec_val);
+	return (num);
 }
